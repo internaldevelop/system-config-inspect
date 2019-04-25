fa8258d3a398b00108723cef4465a44dc12eacd8
baixudong <handan_bxd@163.com>
2019-04-25 08:53:40 +0800

    检测结果柱状图

--- src/views/InspectResultOverview/RiskTypeBar.js
+++ src/views/InspectResultOverview/RiskTypeBar.js
@@ -3,6 +3,7 @@ import ReactEcharts from 'echarts-for-react';
 // import echarts from 'echarts';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import HttpRequest from '../../utils/HttpRequest';
 
 const styles = theme => ({
     root: {
@@ -12,46 +13,106 @@ const styles = theme => ({
 });
 
 class RiskTypeBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedAccID: -1,
+            usersDataReady: false,
+            dimensionsData: [],
+            sourceData: [],
+            seriesBar:[]
+        }
+        this.getTaskStatistics();
+    }
+    
 
     getOption() {
+        const { dimensionsData, sourceData, seriesBar } = this.state;
+                        
         return {
             title: { text: '检测结果' },
             legend: {},
             tooltip: {},
-            xAxis: {
-                data: ['系统服务', '账号配置', '防火墙', '审计配置', 'SQL注入', 'DDos', '自定义']
+            dataset: {
+                dimensions: dimensionsData,
+                // dimensions: ['product', 'Centos', 'Ubuntu'],
+                source: sourceData
+                // source: [
+                //     {product: 'Matcha Latte', 'Centos': 43.3, 'Ubuntu': 85.8},
+                //     {product: 'Milk Tea', 'Centos': 83.1, 'Ubuntu': 73.4},
+                //     {product: 'Cheese Cocoa', 'Centos': 86.4, 'Ubuntu': 65.2},
+                //     {product: 'Walnut Brownie', 'Centos': 72.4, 'Ubuntu': 53.9}
+                // ]
             },
-            yAxis: {
-                type: 'value',
-                axisLabel: {
-                    formatter: '{value}'
-                }
-            },
-            series: [
-                {
-                    name: 'WinServer',
-                    type: 'bar',
-                    barGap: 0,
-                    data: [320, 332, 301, 334, 97, 156, 390]
-                },
-                {
-                    name: 'Kali2',
-                    type: 'bar',
-                    data: [220, 182, 191, 234, 197, 56, 290]
-                },
-                {
-                    name: 'Centos7',
-                    type: 'bar',
-                    data: [150, 232, 201, 154, 67, 256, 190]
-                },
-                {
-                    name: 'Ubuntu16',
-                    type: 'bar',
-                    data: [98, 77, 101, 99, 297, 36, 40]
-                }
-            ]
+            xAxis: {type: 'category'},
+            yAxis: {},
+            series: seriesBar
+            // series: [
+            //     {type: 'bar'},
+            //     {type: 'bar'},
+            // ]
 
         };
+    }
+    
+    getResultsCB = (data) => {
+        const result = data.payload;
+        let strSysType = 'product';
+
+        let policieName = '';  // 策略名
+        let sourceDatas = [];
+        let serBar = [];
+        for (let i = 0; i < result.length; i++) {
+            let osType = result[i].os_type;
+            let pName = result[i].policie_name;
+            
+            if (strSysType.indexOf(osType) < 0) {
+                strSysType += ',';
+                strSysType += osType;
+                
+                let sBar = {'type' : 'bar'};
+                serBar.push(sBar);
+            }
+            if (policieName.indexOf(pName) < 0) {
+                
+                if (0 < policieName.length) {
+                    policieName += ',';
+                }
+                policieName += pName;
+
+                let myMap = {'product' : pName};
+                sourceDatas.push(myMap);
+            }
+        }
+        let sysTypeData = strSysType.split(',');
+        console.log(sourceDatas);
+        
+        for (let i = 0; i < result.length; i++) {
+            let osType = result[i].os_type;
+            let pName = result[i].policie_name;
+            let pNum = result[i].num;
+            
+            sourceDatas.forEach(v=>{
+                
+                if(v.product == pName) {
+                    v[osType] = pNum;
+                }
+            });
+        }
+
+        console.log(sysTypeData);
+        console.log(sourceDatas);
+        console.log(serBar);
+               
+        this.setState({
+            dimensionsData: sysTypeData,
+            sourceData: sourceDatas,
+            seriesBar: serBar
+        });
+    }
+
+    getTaskStatistics() {
+        return HttpRequest.asyncGet(this.getResultsCB, '/tasks/results/statistics');
     }
 
     onChartClick(param,echarts){
