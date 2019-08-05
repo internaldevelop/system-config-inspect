83a48ff15b4972508b02cbccc6b742861ff0fd12
ytwei <249422@qq.com>
2019-08-05 10:08:54 +0800

    Update URL

--- src/global/environment/index.js
+++ src/global/environment/index.js
@@ -6,8 +6,9 @@
 const SYSTEM_TYPE = 1;
 
 const PROTOCOL = 'http';
-// const BASE_URL = '://192.168.87.128:8090/'; // 虚拟机
-const BASE_URL = '://localhost:8090/'; // 本地
+// const BASE_URL = '://192.168.87.128:8090/'; // TQ 虚拟机
+const BASE_URL = '://192.168.207.138:8090/'; // WYT 虚拟机
+// const BASE_URL = '://localhost:8090/'; // 本地
 // const BASE_URL = '://172.16.60.5:8090/'; // 信通所云服务器
 
 export function GetSystemType() {
